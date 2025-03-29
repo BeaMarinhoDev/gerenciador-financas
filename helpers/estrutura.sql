8a1afaf3498e5e4d6dae779aeb2022ff57585311
BeaMarinhoDev <k_beatrizmlc@hotmail.com>
2025-03-29 16:27:16 -0300

    scripts da tabela credits

--- helpers/estrutura.sql
+++ helpers/estrutura.sql
@@ -41,3 +41,13 @@ CREATE TABLE `debits` (
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `credits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_vencimento` date DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
